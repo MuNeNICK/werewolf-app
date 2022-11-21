be7e72fd132c6938d7e24741b704c7ce8e1b3b80
MuNeNiCK <mune0127@munenick.me>
2022-11-21 09:39:59 +0900

    20221121

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,13 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.mysql import INTEGER, VARCHAR
 import pymysql
 from flask_login import LoginManager, login_user
-from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 
-socketio = SocketIO(app, cors_allowed_origins='*')
 
 db = SQLAlchemy(app)
 
@@ -21,4 +19,4 @@ login_manager.init_app(app)
 import views
 
 if __name__ == '__main__':
-    socketio.run(host='localhost', debug=True)
+    app.run(host='localhost', debug=True)
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,7 +1,6 @@
 from flask import *
 from flask_login import *
-from flask_socketio import SocketIO, send, emit
-from app import app, db, socketio
+from app import app, db
 
 
 @app.login_manager.user_loader
@@ -12,38 +11,3 @@ def load_user(user_id):
 def index():
     return jsonify({"content": "あいうえお"})
 
-@app.route('/aaa')
-def aaa():
-    return render_template('index.html')
-
-user_count = 0
-text = ""
-
-
-# ユーザーが新しく接続すると実行
-@socketio.on('connect')
-def connect(auth):
-    global user_count, text
-    user_count += 1
-    # 接続者数の更新（全員向け）
-    emit('count_update', {'user_count': user_count}, broadcast=True)
-    # テキストエリアの更新
-    emit('text_update', {'text': text})
-
-# ユーザーの接続が切断すると実行
-@socketio.on('disconnect')
-def disconnect():
-    global user_count
-    user_count -= 1
-    # 接続者数の更新（全員向け）
-    emit('count_update', {'user_count': user_count}, broadcast=True)
-
-
-# テキストエリアが変更されたときに実行
-@socketio.on('text_update_request')
-def text_update_request(json):
-    global text
-    text = json["text"]
-    # 変更をリクエストした人以外に向けて送信する
-    # 全員向けに送信すると入力の途中でテキストエリアが変更されて日本語入力がうまくできない
-    emit('text_update', {'text': text}, broadcast=True, include_self=False)
